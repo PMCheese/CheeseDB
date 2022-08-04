bdfc5e9a23c8e5abcf97bc288bac9413b18c3af8
PMCheese <alexwong.ca@gmail.com>
2022-08-04 11:44:06 -0400

    test static port for heroku

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use('/api/heroes', heroes);
 ///   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 //}
 
-const port = process.env.PORT || 5000;
+const port = 51245;
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
