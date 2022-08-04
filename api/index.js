2f7628f1f321b4c15797d958f0d2a6b09fc30b25
PMCheese <alexwong.ca@gmail.com>
2022-08-04 14:44:06 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,5 +20,5 @@ app.use('/api/heroes', heroes);
 const port = Number(process.env.PORT)-1 || 5000;
 
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server started on port ${port}`));
 
