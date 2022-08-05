98ffb70b8c09caf47a184f7515b5792909e54137
PMCheese <alexwong.ca@gmail.com>
2022-08-05 01:09:38 -0400

    Add skill info

--- pages/Heroes/_id/index.vue
+++ pages/Heroes/_id/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <div  class = "topstats">
+        
         <h1>{{hero.Hero}}</h1>
         <h2>CP: {{hero.CP}}</h2>
         <h2>Class: {{hero.Class}}</h2>
@@ -14,7 +16,20 @@
         <h2>Effect Resistance: {{hero.EffR}}</h2>
         <h2>Dual Attack Chance: {{hero.DualAttack}}</h2>
 
-    </div>    
+        </div>
+
+        <div class = "midskills">
+        <h1>Skills</h1>
+        <h2>{{hero.S1Name}}</h2>
+        <h3>{{hero.S1Desc}}</h3>
+        <h2>{{hero.S2Name}}</h2>
+        <h3>{{hero.S2Desc}}</h3>
+        <h2>{{hero.S3Name}}</h2>
+        <h3>{{hero.S3Desc}}</h3>
+        </div>  
+       
+    </div>  
+    
 </template>
 
 <script>
@@ -40,5 +55,10 @@ export default {
 </script>
 
 <style>
+
+.topstats{
+    padding: 2rem;
+    padding-bottom: 4rem; 
+}
 
 </style>
