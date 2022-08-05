a9937acce15786a5bdd2a475633f8cf16bec6ea1
PMCheese <alexwong.ca@gmail.com>
2022-08-04 21:18:00 -0400

    Update api-client.js

--- plugins/api-client.js
+++ plugins/api-client.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 
 const api = {
     getData() {
-        return fetch('http:0.0.0.0:5000/api/heroes');
-        //return axios.get("/api/heroes")
+        //return fetch('http:0.0.0.0:5000/api/heroes');
+        return axios.get("/api/heroes")
     }
 }
