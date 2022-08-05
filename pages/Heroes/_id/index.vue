96ac1a975924da497177b5e7f5cdcc5afa4146a2
PMCheese <alexwong.ca@gmail.com>
2022-08-05 00:48:51 -0400

    Update all values

--- pages/Heroes/_id/index.vue
+++ pages/Heroes/_id/index.vue
@@ -2,7 +2,18 @@
     <div>
         <h1>{{hero.Hero}}</h1>
         <h2>CP: {{hero.CP}}</h2>
+        <h2>Class: {{hero.Class}}</h2>
+        <h2>Element: {{hero.Element}}</h2>
+        <h2>Attack: {{hero.Attack}}</h2>
+        <h2>Defence: {{hero.Defence}}</h2>
         <h2>Health: {{hero.Health}}</h2>
+        <h2>Speed: {{hero.Speed}}</h2>
+        <h2>Critical Hit Chance: {{hero.Crit}}</h2>
+        <h2>Critical Hit Damage: {{hero.CritD}}</h2>
+        <h2>Effectiveness: {{hero.Eff}}</h2>
+        <h2>Effect Resistance: {{hero.EffR}}</h2>
+        <h2>Dual Attack Chance: {{hero.DualAttack}}</h2>
+
     </div>    
 </template>
 
